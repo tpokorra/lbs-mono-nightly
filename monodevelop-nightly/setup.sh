6face9e025c213a18dd22af64c7928598940dfe3
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2014-10-04 17:28:26 +0200

    fix tarballs to still include the dlls and exe files

--- monodevelop-nightly/setup.sh
+++ monodevelop-nightly/setup.sh
@@ -32,6 +32,10 @@ function buildTarBall {
     fi
   fi
 
+  # quick fix to include dlls and .exe files in the tarball
+  # reverting commit https://github.com/mono/monodevelop/commit/a6ce3fd8982770e8d72bfdfb1cd8c5d2c11fdd6b
+  sed -i "s#find tarballs/monodevelop#find tarballsDisabled/monodevelop#g" Makefile
+
   # this does not seem to work for CentOS: error: possibly undefined macro: m4_esyscmd_s, need newer autoconf
   make dist || exit 1
 
